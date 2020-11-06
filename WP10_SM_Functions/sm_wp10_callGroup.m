d9740589fc90abeffe5f6f695bdca0e9402237de
Maier <maierp@charite.de>
2020-11-06 12:14:26 +0100

    Update ID to group information

--- WP10_SM_Functions/sm_wp10_callGroup.m
+++ WP10_SM_Functions/sm_wp10_callGroup.m
@@ -7,20 +7,20 @@ function [group,Group]=sm_wp10_callGroup(id)
 %
 % Returns: group (integer) and Group (string) information. 
 
-if (id <=11999 && id>=10000) %% CHECK
-    group=0;
+if (id <=11999 && id>=10000) 
+    group=1;
     Group='YoungKids';
 elseif (id <=12999 && id>=12000)
-    group=1;
-    Group='OldKids';
-elseif (id <=13999 && id>=13000) %% CHECK
     group=2;
+    Group='OldKids';
+elseif (id <=15999 && id>=15000) 
+    group=5;
     Group='YoungAdults';
-elseif (id <=14999 && id>=14000) %% CHECK
-    group=3;
+elseif (id <=16999 && id>=16000) 
+    group=6;
     Group='OldAdults';
 else
-    group=100;
+    group=999;
     Group='F';
     disp('The provided id is out of limits. Group is set to "F"')
 end
