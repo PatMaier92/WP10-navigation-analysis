ea652e85a2b3f71d0d163ef19caf8d5e4af18da0
PatMaier92 <maier_patrizia@web.de>
2020-11-10 15:01:16 +0100

    Add trial_condition option 3 for main_learn trials with no feedback (simple retrieval trials)

--- StarMaze_Script_WP10.m
+++ StarMaze_Script_WP10.m
@@ -171,12 +171,13 @@ M = xlsread(fullfile(folderIn, name),'A:D'); % Reading in data into matrix M
 %% Get info depending on single trial: Feedback, Trial-Type/Condition, Goal & Start Position from trial_results.csv
 sm.sub{p}.session{s}.trial{k}.block=trial_data.block_num(k,1);
 sm.sub{p}.session{s}.trial{k}.trial_num=trial_data.trial_num(k,1);
-sm.sub{p}.session{s}.trial{k}.trial_in_block=trial_data.trial_num_in_block(k,1);       
-sm.sub{p}.session{s}.trial{k}.trial_type=trial_data.trial_type(k,1);
-sm.sub{p}.session{s}.trial{k}.trial_condition=sm_trialCondition(sm.sub{p}.wp,sm.sub{p}.session{s}.trial{k}.trial_type);
+sm.sub{p}.session{s}.trial{k}.trial_in_block=trial_data.trial_num_in_block(k,1);     
 
 sm.sub{p}.session{s}.trial{k}.feedback=trial_data.trial_feedback(k,1);
 [sm.sub{p}.session{s}.trial{k}.fb]=sm_wp10_feedback(sm.sub{p}.session{s}.trial{k}.feedback);
+
+sm.sub{p}.session{s}.trial{k}.trial_type=trial_data.trial_type(k,1);
+sm.sub{p}.session{s}.trial{k}.trial_condition=sm_trialCondition(sm.sub{p}.wp,sm.sub{p}.session{s}.trial{k}.trial_type,sm.sub{p}.session{s}.trial{k}.fb);
 
 sm.sub{p}.session{s}.trial{k}.trial_goal_identity=trial_data.trial_goal_identity(k,1);
 sm.sub{p}.session{s}.trial{k}.trial_goal=trial_data.trial_goal(k,1);
--- WP10_SM_Functions/sm_trialCondition.m
+++ WP10_SM_Functions/sm_trialCondition.m
@@ -1,21 +1,23 @@
-function [TC]=sm_trialCondition(wp,trial_type)
+function [TC]=sm_trialCondition(wp,trial_type,feedback)
 % SM_TRIALCONDITION Function for assinging starmaze trial condition based
 % on work package.
 %
 % Input: 
 % wp is starmaze work package (integer)
 % trial_type is trial condition information (string) 
+% feedback indicates whether goal is visible (yes/no) (boolean)
 %
 % Returns: TC is trial type (integer)
+% 0 = main_learn, 3 = main_retrieval, 1 = allocentric, 2 = egocentric
 
-if wp==3 || wp==6 || wp==31
-    training='training';
-    allo='allo';
-    ego='ego';
-elseif wp==10
+if wp==10
     training='main_learn';
+    % note: main_learn + no Feedback (simple retrieval) is not marked as 
+    % extra category in Starmaze WP10 output files yet.  
     allo='main_allo';
     ego='main_ego';
+else 
+    disp('Wrong work package. Trial condition not determined in sm_TrialCondition.');
 end
 
 TC_training=contains(trial_type,training);
@@ -23,10 +25,15 @@ TC_allo=contains(trial_type,allo);
 TC_ego=contains(trial_type,ego);
 
 if TC_training==1
-    TC=0;
+    if feedback==0
+        TC=3;
+    else
+        TC=0;
+    end
 elseif TC_allo==1
     TC=1;
 elseif TC_ego==1
     TC=2;
 end
+
 end
