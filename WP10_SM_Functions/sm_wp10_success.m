054d4f7c7fbed0cb71dcff366cacb4259806ce09
PatMaier92 <maier_patrizia@web.de>
2020-11-10 15:56:11 +0100

    Minor changes wp10_success

--- WP10_SM_Functions/sm_wp10_success.m
+++ WP10_SM_Functions/sm_wp10_success.m
@@ -1,13 +1,13 @@
-function [success, success_ego, correct_final_alley]=sm_wp10_success(final_distance,final_distance_ego, trial_goal, chosen_goal)
+function [success, success_ego, correct_final_alley]=sm_wp10_success(success_crit, final_distance,final_distance_ego, trial_goal, chosen_goal)
 % SM_WP10_SUCCESS Determines success performance in Starmaze WP10.
 %
 % Input:
-% final_distance, final_distance_ego, trial_goal, chosen_goal are different
-% indicators for behavior. 
+% final_distance, final_distance_ego are distance values (float). 
+% trial_goal and chosen_goal indicate correct and chosen alley(string). 
 %
-% Returns: Different parameters success, success_ego, correct_final_alley. 
+% Returns: success, success_ego and correct_final_alley (boolean). 
  
-if final_distance <= 0.1
+if final_distance <= success_crit
     success=1;
 else
     success=0;
