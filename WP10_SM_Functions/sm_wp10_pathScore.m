e6f27d006f6c5db2a4f39d6b8fd3b92358ae996e
PatMaier92 <maier_patrizia@web.de>
2020-11-10 15:45:32 +0100

    Minor changes comments in wp10_pathScore

--- WP10_SM_Functions/sm_wp10_pathScore.m
+++ WP10_SM_Functions/sm_wp10_pathScore.m
@@ -1,16 +1,16 @@
 function pathScore=sm_wp10_pathScore(alley_zone_out, alley_zone_in, rectangle_zone, triangle_zone)
-% SM_WP10_PATHSCORE Takes exploration information and returns a
-% path score for Starmaze WP10, indicating how many zones have been crossed. 
+% SM_WP10_PATHSCORE Counts how many zones (alleys, triangles, rectanges)
+% were entered in Starmaze WP10. Does not count re-entries in the same zones. 
 %
 % Input: 
-% alley_zone_out, alley_zone_in, rectangle_zone, triangle_zone are zone
-% information
+% alley_zone_out, alley_zone_in, rectangle_zone, triangle_zone have zone
+% information from wp10_coordinatesAlleys/wp10_coordinatesPentagon. 
 %
 % Returns: Path score, indicating how many zones have been crossed
 % (integer).
 
 [row,col]=size(alley_zone_in);
-pathScore=0;
+pathScore=0; % exploration mark
 for c=1:col
     if alley_zone_out(1,c) > 0
         pathScore= pathScore+1;
