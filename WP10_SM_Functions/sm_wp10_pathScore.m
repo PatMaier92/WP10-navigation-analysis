07056857a7bbc81e464913d4f0a35b4b7dc54daf
Maier <maierp@charite.de>
2020-11-11 11:41:12 +0100

    Update pathScore so that re-entries are counted

--- WP10_SM_Functions/sm_wp10_pathScore.m
+++ WP10_SM_Functions/sm_wp10_pathScore.m
@@ -1,6 +1,6 @@
 function pathScore=sm_wp10_pathScore(alley_zone_out, alley_zone_in, rectangle_zone, triangle_zone)
 % SM_WP10_PATHSCORE Counts how many zones (alleys, triangles, rectanges)
-% were entered in Starmaze WP10. Does not count re-entries in the same zones. 
+% were entered in Starmaze WP10. Counts re-entries in the same zones. 
 %
 % Input: 
 % alley_zone_out, alley_zone_in, rectangle_zone, triangle_zone have zone
@@ -8,21 +8,7 @@ function pathScore=sm_wp10_pathScore(alley_zone_out, alley_zone_in, rectangle_zo
 %
 % Returns: pathScore is number of explored zones (integer). 
 
-[row,col]=size(alley_zone_in);
-pathScore=0; % exploration mark
-for c=1:col
-    if alley_zone_out(1,c) > 0
-        pathScore= pathScore+1;
-    end
-    if alley_zone_in(1,c) > 0
-        pathScore= pathScore+1;
-    end
-    if rectangle_zone(1,c) > 0
-        pathScore= pathScore+1;
-    end
-    if triangle_zone(1,c) > 0
-        pathScore= pathScore+1;
-    end
-end
+pathScore = sum(alley_zone_out) + sum(alley_zone_in) + ...
+    sum(rectangle_zone) + sum(triangle_zone); 
 
 end
