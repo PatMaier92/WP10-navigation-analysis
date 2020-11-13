42d12e6b9936633a5d9a86d87c38a64b6734b08a
PatMaier92 <maier_patrizia@web.de>
2020-11-13 10:14:27 +0100

    Add/distinguish beteween pathExplore (no recount) and pathScore (recount)

--- WP10_SM_Functions/sm_wp10_pathScore.m
+++ WP10_SM_Functions/sm_wp10_pathScore.m
@@ -1,14 +1,14 @@
-function pathScore=sm_wp10_pathScore(alley_zone_out, alley_zone_in, rectangle_zone, triangle_zone)
+function pathScore=sm_wp10_pathScore(alley_entry_out, alley_entry_in, rectangle_entry, triangle_entry)
 % SM_WP10_PATHSCORE Counts how many zones (alleys, triangles, rectanges)
 % were entered in Starmaze WP10. Counts re-entries in the same zones. 
 %
 % Input: 
-% alley_zone_out, alley_zone_in, rectangle_zone, triangle_zone have zone
+% alley_entry_out, alley_entry_in, rectangle_entry, triangle_entry have zone
 % information from wp10_coordinatesAlleys/wp10_coordinatesPentagon. 
 %
 % Returns: pathScore is number of explored zones (integer). 
 
-pathScore = sum(alley_zone_out) + sum(alley_zone_in) + ...
-    sum(rectangle_zone) + sum(triangle_zone); 
+pathScore = sum(alley_entry_out) + sum(alley_entry_in) + ...
+    sum(rectangle_entry) + sum(triangle_entry); 
 
 end
