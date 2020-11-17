d9d190bf60fa41da2769da8e0ddf6a6f4a1bb881
Maier <maierp@charite.de>
2020-11-17 14:38:01 +0100

    Restructure output: only two data sheets for main data and supporting variables

--- WP10_SM_Functions/sm_wp10_table_allTrials.m
+++ WP10_SM_Functions/sm_wp10_table_allTrials.m
@@ -1,4 +1,4 @@
-function sm_wp10_table_allTrials(folderIn, new_file, col_header, col_header_2, col_header_3, col_header_4)
+function sm_wp10_table_allTrials(folderIn, new_file, col_header, col_header_2, col_header_3)
 % SM_WP10_TABLE_ALLTRIALS Write table for all trials of the current ID 
 % for Starmaze WP10. Save in individual folder. 
 %
@@ -13,24 +13,20 @@ for k=1:numel(files)
     name = files{k};
     row=k+1;
     row=int2str(row);
-    x=['A' row ':AD' row];
-    y=['A' row ':AI' row];
-    z=['A' row ':CR' row];
+    x=['A' row ':AZ' row];
+    y=['A' row ':CR' row];
     
     % reading in data for table per ID
-    d_path=xlsread(fullfile(folderIn, name),'path','A2:AD2'); 
-    d_explore=xlsread(fullfile(folderIn, name),'exploration','A2:AI2');
-    d_support=xlsread(fullfile(folderIn, name),'support_vars','A2:CR2');
+    d_data=xlsread(fullfile(folderIn, name),'data_vars','A2:AZ2'); 
+    d_support=xlsread(fullfile(folderIn, name),'support_vars','A2:CR2'); %%
     
     % header
-    xlswrite(new_file,[col_header col_header_2 ],'path','A1');
-    xlswrite(new_file,[col_header col_header_3 ],'exploration','A1');
-    xlswrite(new_file,[col_header col_header_4 ],'support_vars','A1');
+    xlswrite(new_file,[col_header col_header_2 ],'data_vars','A1');
+    xlswrite(new_file,[col_header col_header_3 ],'support_vars','A1');
 
     % data
-    xlswrite(new_file,d_path,'path',x);
-    xlswrite(new_file,d_explore,'exploration',y);
-    xlswrite(new_file,d_support,'support_vars',z);
+    xlswrite(new_file,d_data,'data_vars',x);
+    xlswrite(new_file,d_support,'support_vars',y);
 end 
 
 end
