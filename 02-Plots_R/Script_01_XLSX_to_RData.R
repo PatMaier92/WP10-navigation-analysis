dee7d64bf63495243bc9a2c05e389762d660dadf
PatMaier92 <maier_patrizia@web.de>
2020-11-25 15:59:37 +0100

    Add block, trial_in_block, trial_in_cond and trial_in_block_in_cond info for plotting. Should ultimately be added to Matlab scripts

--- 02-Plots_R/Script_01_XLSX_to_RData.R
+++ 02-Plots_R/Script_01_XLSX_to_RData.R
@@ -3,10 +3,12 @@
 ### Author: Patrizia Maier                                ###
 
 
+
 ## install packages
 # install.packages("readxl")
 # install.packages("tidyverse")
 # install.packages("arsenal")
+
 
 
 ## get packages
@@ -15,18 +17,19 @@ library(tidyverse)
 library(arsenal)
 
 
+
 ## read xlsx data 
 date = "201118"
 in_file <- paste("../WP10_data/WP10_results/WP10_results_table_", date, ".xlsx", sep="")
 sm_trial_data <- read_xlsx(in_file, sheet = "data_vars", col_names = T)
-# sm_support <- read_xlsx(in_file, sheet = "support_vars", col_names = T)
 rm(date, in_file)
+
 
 
 ## tidy data 
 # sort correct order with arrange()
 sm_trial_data <- arrange(sm_trial_data, id, session, trial) 
-# sm_support <- arrange(sm_support, id, session, trial)
+
 
 # add factor information
 sm_trial_data$sex <- factor(sm_trial_data$sex, levels=c(1, 2), 
@@ -42,23 +45,76 @@ sm_trial_data$goal_identity <- factor(sm_trial_data$goal_identity, levels=c(1, 2
 sm_trial_data$search_strategy_no <- factor(sm_trial_data$search_strategy_no, levels=c(1,2,3,4,5,6), 
                                      labels=c("direct_strategy","central_focus","reoriented","serial","random","unclassified"))
 
+# add variable with block info (ADD THIS TO MATLAB)
+sm_trial_data <- sm_trial_data  %>%
+  mutate(block=ifelse(session==2 & trial %in% 1:5, 1,
+                      ifelse(session==2 & trial %in% 6:10, 2,
+                             ifelse(session==2 & trial %in% 11:15, 3,
+                                    ifelse(session==1 & trial %in% 1:13, 1,
+                                           ifelse(session==1 & trial %in% 14:26, 2,
+                                                  ifelse(session==1 &trial %in% 27:39, 3, 4)))))))
+sm_trial_data <- sm_trial_data %>%
+  mutate(block=ifelse(block=="4" & goal_position==sm_trial_data$goal_position[1], 1,
+                      ifelse(block=="4" & goal_position==sm_trial_data$goal_position[14], 2,
+                             ifelse(block=="4" & goal_position==sm_trial_data$goal_position[27], 3, block))))
 
-## optional: add summary data (--> do this in Matlab already in sm_wp10_summary.m)
-# sm_sum_data <- sm_trial_data %>% 
-#   group_by(group, session, trial_condition) %>%
-#   summarise(m_success=mean(success),
-#             sd_success=sd(success),
-#             m_finalDist=mean(final_distance_to_goal_abs),
-#             sd_finalDist=sd(final_distance_to_goal_abs),
-#             m_directPath=mean(direct_path),
-#             sd_directPath=sd(direct_path),
-#             m_path=mean(path_abs),
-#             sd_path=sd(path_abs))
+
+# add variable with trial in block info (ADD THIS TO MATLAB)
+assign_trial_in_block <- function(s, b, t){
+  
+  orig_trial <- unique(sm_trial_data$trial[(sm_trial_data$session==s & sm_trial_data$block==b)])
+  index <- which(orig_trial==t)
+  
+  return (index)
+}
+
+sm_trial_data <- sm_trial_data %>%
+  rowwise() %>% 
+  mutate(trial_in_block=assign_trial_in_block(session, block, trial))
+
+# sm_trial_data_g <- sm_trial_data %>%
+#   mutate(trial_in_block=purrr::pmap_dbl(list(session, block, trial), assign_trial_in_block))
+
+
+# add variable with trial in condition info (ADD THIS TO MATLAB)
+assign_trial_in_cond <- function(s, c, t){
+  
+  orig_trial <- unique(sm_trial_data$trial[(sm_trial_data$session==s & sm_trial_data$trial_condition==c)])
+  index <- which(orig_trial==t)
+  
+  return (index)
+}
+
+sm_trial_data <- sm_trial_data %>%
+  rowwise() %>% 
+  mutate(trial_in_cond=assign_trial_in_cond(session, trial_condition, trial))
+
+# sm_trial_data <- sm_trial_data %>%
+#   mutate(trial_in_cond=purrr::pmap_dbl(list(session, trial_condition, trial), assign_trial_in_cond))
+
+
+# add variable with trial in block in condition info (ADD THIS TO MATLAB)
+assign_trial_in_block_in_cond <- function(s, b, c, t){
+  
+  orig_trial <- unique(sm_trial_data$trial[(sm_trial_data$session==s & sm_trial_data$block==b & sm_trial_data$trial_condition==c)])
+  index <- which(orig_trial==t)
+  
+  return (index)
+}
+
+sm_trial_data <- sm_trial_data %>%
+  rowwise() %>% 
+  mutate(trial_in_block_in_cond=assign_trial_in_block_in_cond(session, block, trial_condition, trial))
+
+# sm_trial_data <- sm_trial_data %>%
+#   mutate(trial_in_block_in_cond=purrr::pmap_dbl(list(session, block, trial_condition, trial), assign_trial_in_block_in_cond))
+
 
 
 ## save as RData
 out_file <- "../WP10_data/WP10_results/WP10_results_table.RData"
 save(sm_trial_data, file=out_file)
+
 
 
 ## clear workspace
