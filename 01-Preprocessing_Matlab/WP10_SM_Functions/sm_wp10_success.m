aeab852886759cd60e6bfdbd38adfeb05d37d381
PatMaier92 <maier_patrizia@web.de>
2021-05-17 17:08:42 +0200

    Update success function

--- 01-Preprocessing_Matlab/WP10_SM_Functions/sm_wp10_success.m
+++ 01-Preprocessing_Matlab/WP10_SM_Functions/sm_wp10_success.m
@@ -1,33 +1,33 @@
-function [success, success_ego, correct_final_alley, correct_final_alley_ego]=sm_wp10_success(success_crit,...
-    final_distance,final_distance_ego, trial_goal, chosen_goal, ego_goal)
+function [success_allo, success_ego, correct_final_alley_allo, correct_final_alley_ego]=sm_wp10_success(success_crit,...
+    final_distance_allo, final_distance_ego, allo_alley, chosen_alley_int, ego_alley)
 % SM_WP10_SUCCESS Determines success performance in Starmaze WP10.
 %
 % Input:
-% final_distance, final_distance_ego are distance values (float). 
-% trial_goal and chosen_goal indicate correct and chosen alley(string).
-% ego_alley indicates egocentric alley number. 
+% final_distance_allo, final_distance_ego are distance values (float). 
+% allo_alley, ego_alley and chosen_goal_int indicate correct and chosen
+% alley integers. 
 %
 % Returns: success, success_ego, correct_final_alley, correct_final_alley_ego (boolean). 
  
-if final_distance <= success_crit
-    success=1;
+if final_distance_allo <= success_crit
+    success_allo=1;
 else
-    success=0;
+    success_allo=0;
 end
 
-if final_distance_ego <= 0.1
+if final_distance_ego <= success_crit
     success_ego=1;
 else
     success_ego=0;
 end
 
-if contains(trial_goal, chosen_goal)
-    correct_final_alley=1;
+if allo_alley==chosen_alley_int
+    correct_final_alley_allo=1;
 else
-    correct_final_alley=0;
+    correct_final_alley_allo=0;
 end
 
-if contains(ego_goal, chosen_goal)
+if ego_alley==chosen_alley_int
     correct_final_alley_ego=1;
 else
     correct_final_alley_ego=0;
