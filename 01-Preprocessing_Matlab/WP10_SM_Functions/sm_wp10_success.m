085cd69fa8bf5d2709e78819f77a1f505cbc8ef7
PatMaier92 <maier_patrizia@web.de>
2021-05-17 13:35:30 +0200

    Minor change, add egocentric goal alley. Needs to be tested.

--- 01-Preprocessing_Matlab/WP10_SM_Functions/sm_wp10_success.m
+++ 01-Preprocessing_Matlab/WP10_SM_Functions/sm_wp10_success.m
@@ -1,11 +1,13 @@
-function [success, success_ego, correct_final_alley]=sm_wp10_success(success_crit, final_distance,final_distance_ego, trial_goal, chosen_goal)
+function [success, success_ego, correct_final_alley, correct_final_alley_ego]=sm_wp10_success(success_crit,...
+    final_distance,final_distance_ego, trial_goal, chosen_goal, ego_goal)
 % SM_WP10_SUCCESS Determines success performance in Starmaze WP10.
 %
 % Input:
 % final_distance, final_distance_ego are distance values (float). 
-% trial_goal and chosen_goal indicate correct and chosen alley(string). 
+% trial_goal and chosen_goal indicate correct and chosen alley(string).
+% ego_alley indicates egocentric alley number. 
 %
-% Returns: success, success_ego and correct_final_alley (boolean). 
+% Returns: success, success_ego, correct_final_alley, correct_final_alley_ego (boolean). 
  
 if final_distance <= success_crit
     success=1;
@@ -23,6 +25,12 @@ if contains(trial_goal, chosen_goal)
     correct_final_alley=1;
 else
     correct_final_alley=0;
+end
+
+if contains(ego_goal, chosen_goal)
+    correct_final_alley_ego=1;
+else
+    correct_final_alley_ego=0;
 end
  
 end
