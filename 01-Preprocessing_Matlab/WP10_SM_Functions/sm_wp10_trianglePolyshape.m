a417257e4685a8a58a0e18682d9348d4a5abd231
PatMaier92 <maier_patrizia@web.de>
2021-05-17 14:54:20 +0200

    Add comments to functions

--- 01-Preprocessing_Matlab/WP10_SM_Functions/sm_wp10_trianglePolyshape.m
+++ 01-Preprocessing_Matlab/WP10_SM_Functions/sm_wp10_trianglePolyshape.m
@@ -1,9 +1,11 @@
 function [tri_x,tri_y,tri]=sm_wp10_trianglePolyshape(alleyNo,alley_x, alley_y, pentagon_x, pentagon_y)
 % SM_WP10_TRIANGLEPOLYSHAPE Creates a polyshape of Starmaze WP10 triangles. 
 %
-% Input: ?
+% Input: alleyNo is integer with number of available alleys, alley_x and
+% alley_y are arrays with x-/y-coordinates of alley corners, pentagon_y and
+% pentagon_y are vectors with x-/y-coordinates of inner pentagon. 
 %
-% Returns: ?
+% Returns: triangle polyshapes
 
 for alley=1:alleyNo
     tri_x(:,alley)=[alley_x(4,alley);alley_x(3,alley);pentagon_x(1,alley);alley_x(4,alley)];
