4a2195361e0349bf0ced7d0a84335582cbd10920
PatMaier92 <maier_patrizia@web.de>
2022-02-16 13:48:58 +0100

    Update post-navigation memory processing

--- 01-Preprocessing_Matlab/Script_03_PostTest_Preprocessing_WP10.m
+++ 01-Preprocessing_Matlab/Script_03_PostTest_Preprocessing_WP10.m
@@ -1,271 +1,195 @@
-%% Preparation
 clear; close all; clc; format long;
 addpath(genpath(pwd)) % add subfolder functions to path
 
 %% PostTest Data Processing
-% @ date 20210521 update by @ Patrizia Maier & tracked by git 
+% @ date 2021-05-21 @ author Patrizia Maier & tracked by git 
 % for starmaze version WP10 Frankfurt
 
-% The script requires .csv as input-files. 
-% for trial info trial_results.csv is required
-% TBD: GMDA files 
+% The script requires .csv files as input.
+% One trial_results file with general information (id, session, condition).
 
-% Script starts with input: 
-%   1. Provide the span of subjects you would like to analyse, start with
-%   the first subjectnumber, end with the last subjectnumber (Attention:
-%   the folder with S001 has to be in a folder that is named after the
-%   subjectnumber)
+% Block 1: Set up input/output folders, Starmaze and Practise environment
+% Block 2: Data analysis
+% Block 3: Write data to xlsx file
 
-%% Block 1: Input/output
+%% Block 1: Set up input/output folders
+%% data input folder and participant information
+[data_folder]  = setInputPath(); % provide folder with all raw data
+[participant_start,participant_end] = setParticipants(); % provide participant range
+s=4; % session default
 
-%% Get folders and subject list 
-% folder with all input data 
-[dataFolder]=sm_inputPath(); 
-
-% folder for output results 
-folderOut2=[dataFolder '\WP10_results\PostTest'];
-if ~exist(folderOut2, 'dir')
-    mkdir(folderOut2);
-    disp('Your outputfolder for results didn''t exist, a new folder was created')
+%% result folder
+result_folder=[data_folder '\WP10_results'];
+if ~exist(result_folder, 'dir')
+    mkdir(result_folder);
+    disp('Your output folder did not exist, a new folder was created.')
 end
 
-% load/save data
-targetFileName         = '\wp10_results_table_posttest.mat';
-if isfile(fullfile(folderOut2, targetFileName))
-    load(fullfile(folderOut2, targetFileName))
+%% load data table or create new one 
+% load existing data
+file_name         = '\wp10_results_post_nav.mat';
+file_path         = fullfile(result_folder, file_name);
+if isfile(file_path)
+    load(file_path)
 end
-save(fullfile(folderOut2, targetFileName));
- 
-if ~exist('pt.sub')
-    pt.sub={};
-end
-[row,participant]=size(pt.sub);
-p=participant+1;
 
-[subject_start,subject_end]=sm_inputSubjectsNo();
-session=4; % session default
-
-wp=10; % work package default 
+% initialize data if non-existing
+if ~exist('pt','var')
+    pt=[];
+    p=1; % initialize participant index   
+else
+    p=0; % dummy value 
+end 
  
 %% Block 2: Data analysis
-for subject=subject_start:subject_end
-    
-%     % TBD: add this
-%     % check input folder
-%     folderIn=[inputFolder '\' num2str(subj) '\' finalFolderString];
-%     if ~exist(folderIn, 'dir')
-%         fprintf('Subject %d, session %d does not exist. Continue with next folder.\n', subj, session);
-%         continue;
-%     end
-%     %
-    
-    pstr=['p',num2str(subject)];
-    sstr=['s',num2str(session)];
-    [finalFolderString]=sm_wp10_getFolderstring(session);
-    [folderIn,~]=sm_wp10_folder(dataFolder,subject,finalFolderString);
-    folderOut1=[dataFolder '\' num2str(subject) '\results_post_test'];
-    if ~exist(folderOut1, 'dir')
-        mkdir(folderOut1);
-        disp('Your individual outputfolder for results didn''t exist, a new folder was created')
-    end
-    
-    % save data
-    targetFileName_Subject         = ['\' num2str(subject) '_results_table_post.mat'];
-    save(fullfile(folderOut1, targetFileName_Subject));
-
-    % read-in trial file 
-    opts = detectImportOptions([folderIn, '\trial_results.csv']);
-    opts = setvaropts(opts,'timestamp','InputFormat','MM/dd/uuuu hh:mm:ss aa'); 
-    trial_data=readtable([folderIn, '\trial_results.csv'],opts);
-    
-    %% Participant,  Workpackage, Group information
-    s=session; % s=session --> row, p=participant --> coloumn
-    pt.sub{p}.session{s}.session=s;
-    pt.sub{p}.wp=wp; 
-    pt.sub{p}.id=subject;
-    [pt.sub{p}.group,pt.sub{p}.Group,pt.sub{p}.sex,pt.sub{p}.Sex]=sm_wp10_callGroup(pt.sub{p}.id); % provide group information
-    pt.sub{p}.session{s}.session_duration=round(minutes(trial_data.timestamp(4,1) - trial_data.timestamp(1,1))); % provide session duration
-
-    pt.sub{p}.session{s}.lm_MB=trial_data.lm_MB{1,1}(1:2);
-    pt.sub{p}.session{s}.lm_MD=trial_data.lm_MD{1,1}(1:2);
-    pt.sub{p}.session{s}.lm_MF=trial_data.lm_MF{1,1}(1:2);
-    pt.sub{p}.session{s}.lm_MH=trial_data.lm_MH{1,1}(1:2);
-    pt.sub{p}.session{s}.lm_MJ=trial_data.lm_MJ{1,1}(1:2);
-    pt.sub{p}.session{s}.obj_MA=trial_data.obj_MA{1,1}(1:2);
-    pt.sub{p}.session{s}.obj_MC=trial_data.obj_MC{1,1}(1:2);
-    pt.sub{p}.session{s}.obj_MI=trial_data.obj_MI{1,1}(1:2);
-
-%% Trial-wise data analysis
-for k=1:size(trial_data,1)
-    %% General info
-    pt.sub{p}.session{s}.trial{k}.trial_num=trial_data.trial_num(k,1);
-    pt.sub{p}.session{s}.trial{k}.trial_type=trial_data.trial_num(k,1); % same as trial number, string info in R
-    
-    %% Time analysis using timestamp
-    b=trial_data.end_time(k,1); a=trial_data.start_time(k,1);
-    pt.sub{p}.session{s}.trial{k}.result.time=sm_time(a,b); % total amount of time
-
-    fprintf('Time analysis done for %d, session %d, file no %d.\n', subject, session, k);
-
-    %% Performance analysis
-    if k==1
-        % shape recognition
-        if trial_data.suc_1{k,1}=="True"
-            points=1;
-        else 
-            points=0;
+for id=participant_start:participant_end
+    %% set participant index
+    if p~=1 
+        % check if ID exists in data 
+        p_ind = find([pt.id]==id); 
+        if isempty(p_ind) % if not: append participant data    
+            [~,n]=size(pt);
+            p=n+1; clear n p_ind; 
+        else % if yes: overwrite participant data
+            p=p_ind; clear p_ind;
+            fprintf('Data for participant %d already existed and is overwritten now.\n', id);
         end
-        
-        pt.sub{p}.session{s}.trial{k}.result.score=points;
-            
-        fprintf('Shape recognition done for %d, session %d, file no %d.\n', subject, session, k);
-    elseif k==2
-        % landmark recogntion
-        % setup
-        points=0; 
-        corr_list={ 'Mountain-House', 'Forest-House', 'Tower', 'Forest', 'Mountain' };
-            
-        % get chosen landmarks
-        obj_1=trial_data.obj_1{k,1}(4:end);
-        obj_1=strsplit(obj_1, '_');
-
-        obj_2=trial_data.obj_2{k,1}(4:end);
-        obj_2=strsplit(obj_2, '_');
-
-        obj_3=trial_data.obj_3{k,1}(4:end);
-        obj_3=strsplit(obj_3, '_');
-
-        obj_4=trial_data.obj_4{k,1}(4:end);
-        obj_4=strsplit(obj_4, '_');
-
-        obj_5=trial_data.obj_5{k,1}(4:end);
-        obj_5=obj_5(4:end);
-        obj_5=strsplit(obj_5, '_');
-
-        % save in cell list 
-        obj_list=[obj_1; obj_2; obj_3; obj_4; obj_5]; 
-        
-        % score 1 point for correct item
-        log_cor=contains(obj_list(:,2), 'corr');
-        points=points+sum(log_cor);
-        
-        % remove correct items from list --> similar items only score points
-        % if not same semantic category 
-        corr_list(ismember(corr_list,obj_list(log_cor,1))) = [];
-        
-        % score 0.5 points for similar but semantically distinct items 
-        % no points for dissimilar items 
-        points=points+(sum(ismember(obj_list(:,1), corr_list) & contains(obj_list(:,2), 'sim'))/2);
-        
-        % calculate average 
-        points=points/5; 
-
-        % save 
-        pt.sub{p}.session{s}.trial{k}.result.score=points;
-        
-        fprintf('Landmark recognition done for %d, session %d, file no %d.\n', subject, session, k);
-    elseif k==3
-        % goal recognition
-        % setup
-        points=0; 
-        corr_list={ trial_data.obj_MA{k,1}, trial_data.obj_MC{k,1}, trial_data.obj_MI{k,1} };
-            
-        % get chosen goals
-        obj_1=trial_data.obj_1{k,1};
-
-        obj_2=trial_data.obj_2{k,1};
-
-        obj_3=trial_data.obj_3{k,1};
-
-        % save in cell list 
-        obj_list={ obj_1; obj_2; obj_3 }; 
-        
-        % score 1 point for correct item
-        log_cor=contains(obj_list, corr_list);
-        points=points+sum(log_cor);
-        
-        % calculate average 
-        points=points/3; 
-
-        % save 
-        pt.sub{p}.session{s}.trial{k}.result.score=points;
-        
-        fprintf('Goal recognition done for %d, session %d, file no %d.\n', subject, session, k);
-    else
-        % landmark positioning --> GMDA TBD
-        % save 
-        pt.sub{p}.session{s}.trial{k}.result.score=999;
-        
     end 
     
-    % save object info 
-    pt.sub{p}.session{s}.obj_1=0;
-    pt.sub{p}.session{s}.obj_2=0;
-    pt.sub{p}.session{s}.obj_3=0;
-    pt.sub{p}.session{s}.obj_4=0;
-    pt.sub{p}.session{s}.obj_5=0;
-    
-    if k==1
-        pt.sub{p}.session{s}.obj_1=trial_data.obj_1{1,1}(1);
-    elseif k==2 
-        pt.sub{p}.session{s}.obj_1=trial_data.obj_1{2,1}(1:2);
-        pt.sub{p}.session{s}.obj_2=trial_data.obj_2{2,1}(1:2);
-        pt.sub{p}.session{s}.obj_3=trial_data.obj_3{2,1}(1:2);
-        pt.sub{p}.session{s}.obj_4=trial_data.obj_4{2,1}(1:2);
-        pt.sub{p}.session{s}.obj_5=trial_data.obj_5{2,1}(1:2);
-    elseif k==3 
-      pt.sub{p}.session{s}.obj_1=trial_data.obj_1{3,1}(1:2);
-      pt.sub{p}.session{s}.obj_2=trial_data.obj_2{3,1}(1:2);
-      pt.sub{p}.session{s}.obj_3=trial_data.obj_3{3,1}(1:2);
+    %% set individual input folder
+    % input folder
+    input_folder=[data_folder '\' num2str(id) '\S00' num2str(s)];
+    if ~exist(input_folder, 'dir')
+        fprintf('Folder for participant %d, session %d does not exist. Continue with next iteration.\n', id, s);
+        continue;
+    end
+
+    %% read-in trial results file
+    opts=detectImportOptions([input_folder, '\trial_results.csv']);
+    opts=setvaropts(opts,'timestamp','TreatAsMissing','na','Type','datetime','InputFormat','MM/dd/uuuu hh:mm:ss aa');
+    trial_data=readtable([input_folder, '\trial_results.csv'], opts); clear opts;
+      
+    %% set participant, group and session duration information 
+    pt(p).id=id; 
+    [pt(p).group,pt(p).group_s, pt(p).sex,pt(p).sex_s]=setGroupSexInfo(pt(p).id);
+    pt(p).session_duration=round(minutes(trial_data.timestamp(4,1) - trial_data.timestamp(1,1))); 
+
+    % loop over trials
+    for k=1:size(trial_data,1)
+        %% general info
+        % trial
+        pt(p).trial(k).trial_num=trial_data.trial_num(k,1); 
+        % correct items 
+        pt(p).trial(k).lm_MB=trial_data.lm_MB{1,1}(1:2);
+        pt(p).trial(k).lm_MD=trial_data.lm_MD{1,1}(1:2);
+        pt(p).trial(k).lm_MF=trial_data.lm_MF{1,1}(1:2);
+        pt(p).trial(k).lm_MH=trial_data.lm_MH{1,1}(1:2);
+        pt(p).trial(k).lm_MJ=trial_data.lm_MJ{1,1}(1:2); 
+        pt(p).trial(k).obj_MA=trial_data.obj_MA{1,1}(1:2);
+        pt(p).trial(k).obj_MC=trial_data.obj_MC{1,1}(1:2);
+        pt(p).trial(k).obj_MI=trial_data.obj_MI{1,1}(1:2);
+        % dummy for chosen items 
+        pt(p).trial(k).obj_1=999;
+        pt(p).trial(k).obj_2=999;
+        pt(p).trial(k).obj_3=999;
+        pt(p).trial(k).obj_4=999;
+        pt(p).trial(k).obj_5=999;
+        
+        %% time analysis
+        b=trial_data.end_time(k,1); a=trial_data.start_time(k,1);
+        pt(p).trial(k).time=computeTime(a,b); clear a b; 
+        % fprintf('Time analysis done for %d, file no %d.\n', id, k);
+        
+        %% performance analysis
+        if k==1 % SHAPE RECOGNITION
+            if trial_data.suc_1{k,1}=="True"
+                points=1;
+            elseif trial_data.suc_1{k,1}=="na"
+                points=999;
+            else
+                points=0;
+            end
+            % save score
+            pt(p).trial(k).score=points;
+            
+            % save item info
+            if trial_data.suc_1{k,1}=="na"
+                pt(p).trial(k).obj_1=999; 
+            else 
+                pt(p).trial(k).obj_1=trial_data.obj_1{k,1}(1);
+            end
+            % fprintf('Shape recognition done for %d, file no %d.\n', id, k);
+            
+        elseif k==2 % LANDMARK RECOGNITION 
+            % setup
+            corr_list={ 'Mountain-House', 'Forest-House', 'Tower', 'Forest', 'Mountain' };
+            
+            % get chosen items 
+            obj_1=trial_data.obj_1{k,1}(4:end); obj_1=strsplit(obj_1, '_'); 
+            obj_2=trial_data.obj_2{k,1}(4:end); obj_2=strsplit(obj_2, '_');
+            obj_3=trial_data.obj_3{k,1}(4:end); obj_3=strsplit(obj_3, '_');
+            obj_4=trial_data.obj_4{k,1}(4:end); obj_4=strsplit(obj_4, '_');
+            obj_5=trial_data.obj_5{k,1}(4:end); obj_5=strsplit(obj_5, '_');
+            obj_list=[obj_1; obj_2; obj_3; obj_4; obj_5];
+            
+            % score 1 point per correct item
+            log_corr=contains(obj_list(:,2),'corr');
+            points=sum(log_corr);
+            
+            % score 0.5 points per similar but semantically distinct items
+            % remove correct items from list (to determine semantic distinctness) 
+            corr_list(ismember(corr_list,obj_list(log_corr,1))) = [];
+            % re-calculate score
+            points=points+(sum(ismember(obj_list(:,1),corr_list) & contains(obj_list(:,2),'sim'))/2);         
+            
+            % save average
+            pt(p).trial(k).score=points/5;
+            
+            % save item info 
+            pt(p).trial(k).obj_1=trial_data.obj_1{k,1}(1:2);
+            pt(p).trial(k).obj_2=trial_data.obj_2{k,1}(1:2);
+            pt(p).trial(k).obj_3=trial_data.obj_3{k,1}(1:2);
+            pt(p).trial(k).obj_4=trial_data.obj_4{k,1}(1:2);
+            pt(p).trial(k).obj_5=trial_data.obj_5{k,1}(1:2);
+            % fprintf('Landmark recognition done for %d, file no %d.\n', id, k);
+            
+        elseif k==3 % GOAL RECOGNITION
+            % setup
+            corr_list={ trial_data.obj_MA{k,1}, trial_data.obj_MC{k,1}, trial_data.obj_MI{k,1} };
+            
+            % get chosen items
+            obj_1=trial_data.obj_1{k,1};   
+            obj_2=trial_data.obj_2{k,1};
+            obj_3=trial_data.obj_3{k,1};
+            obj_list={ obj_1; obj_2; obj_3 };
+            
+            % score 1 point per correct item
+            points=sum(contains(obj_list, corr_list));
+
+            % save average
+            pt(p).trial(k).score=points/3; 
+            
+            % save item information
+            pt(p).trial(k).obj_1=obj_1(1:2);
+            pt(p).trial(k).obj_2=obj_2(1:2);
+            pt(p).trial(k).obj_3=obj_3(1:2);
+            % fprintf('Goal recognition done for %d, file no %d.\n', id, k);
+           
+        else % POSITIONING: done externally with GMDA software
+            pt(p).trial(k).score=999;
+        end
+
+        clear points obj* *corr*; 
     end
     
-    % save data
-    save(fullfile(folderOut1, targetFileName_Subject),'pt', '-append'); 
-
-    %% Block 3: Writing data ---> result sheet XLSX for single trials %%
-    % header
-    col_header={'wp','date_analysis','id','sex','group','session','session_duration',...
-        'trial','trial_condition' };
-
-    % main variables
-    col_header_2={'score','time',...
-        'obj_1', 'obj_2', 'obj_3', 'obj_4', 'obj_5',...
-        'lm_MB', 'lm_MD', 'lm_MF', 'lm_MH', 'lm_MJ',...
-        'obj_MA', 'obj_MC', 'obj_MI' };
-
-    % name of excel-file
-    Trial=num2str(pt.sub{p}.session{s}.trial{k}.trial_num);
-    Session=num2str(pt.sub{p}.session{s}.session);
-    group_var=[pt.sub{p}.wp string(yyyymmdd(datetime)) pt.sub{p}.id pt.sub{p}.sex pt.sub{p}.group ...  
-        pt.sub{p}.session{s}.session pt.sub{p}.session{s}.session_duration ...
-        pt.sub{p}.session{s}.trial{k}.trial_num pt.sub{p}.session{s}.trial{k}.trial_type ];
-    file_name = ['\results_' num2str(wp) '_' pt.sub{p}.Group '_' num2str(subject) '_' Session '_' Trial '.xls'];
-    new_file = fullfile(folderOut1, file_name);
-
-    % write data
-    xlswrite(new_file,strrep([group_var ...
-        pt.sub{p}.session{s}.trial{k}.result.score pt.sub{p}.session{s}.trial{k}.result.time ...
-        pt.sub{p}.session{s}.obj_1 pt.sub{p}.session{s}.obj_2 pt.sub{p}.session{s}.obj_3 ...
-        pt.sub{p}.session{s}.obj_4 pt.sub{p}.session{s}.obj_5 ...
-        pt.sub{p}.session{s}.lm_MB pt.sub{p}.session{s}.lm_MD pt.sub{p}.session{s}.lm_MF ...
-        pt.sub{p}.session{s}.lm_MH pt.sub{p}.session{s}.lm_MJ ...
-        pt.sub{p}.session{s}.obj_MA pt.sub{p}.session{s}.obj_MC pt.sub{p}.session{s}.obj_MI ],'.', ','),'post_vars','A2');
-
-    xlswrite(new_file,[col_header col_header_2],'post_vars','A1');
-
+    save(file_path, 'pt');
+    fprintf('Analysis done for %d.\n', id);
+    clear k trial_data input_folder; 
+    p=0; % dummy value
 end
 
-%% Write summaries for a selection of variables
-new_name2 = [pt.sub{p}.Group '_' num2str(pt.sub{p}.id)  '_results_post'];
-new_file = fullfile(folderOut2, new_name2);
-sm_wp10_table_allTrials_post(folderOut1, new_file, col_header, col_header_2, 'X', 'post_vars');
+%% Block 3: Write data to xlsx file
+% [data_folder]  = setInputPath();
+% writeTableToXLSX(data_folder); 
 
-p=p+1;
-
-end
-
-%% Save data
-targetFilePath         = [folderOut2, targetFileName];  
-save(targetFilePath, 'pt', '-append')
-
-clear
+clear; 
