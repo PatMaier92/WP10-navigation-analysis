47fcb92f1b16404ca7fbd30bf92738b2cb19f8ba
PatMaier92 <maier_patrizia@web.de>
2023-02-27 17:13:57 +0100

    Minor modifications and clean-up

--- 04-PLSC/Script_01_RData_to_MAT.R
+++ 04-PLSC/Script_01_RData_to_MAT.R
@@ -61,66 +61,8 @@ pt_data <- pt_data %>%
 # ::: FUNCTION FOR DATA WRANGLING ::: #
 # ------------------------------------------------------------------------------
 
-# # process data for plsc analysis 
-# data_for_plsc_old <- function(d_sm, d_pt, ms_session, ms_condition, nav_session, nav_condition, nav_trials=1:8){
-#   
-#   # data wrangling 
-#   # navigation
-#   d_nav <- d_sm %>% 
-#     filter(session %in% nav_session, condition %in% nav_condition, trial_in_block %in% nav_trials) %>% 
-#     group_by(id) %>% 
-#     summarise_at(vars(time, excess_path_length, excess_target_distance, initial_rotation_velocity), mean, na.rm=T)
-#   
-#   # memory 
-#   if (ms_session %in% c(1, 2)) {
-#     # memory score
-#     d_ms <- d_sm %>% 
-#       filter(session %in% ms_session, condition %in% ms_condition) %>% 
-#       group_by(id, group) %>% 
-#       summarise_at(vars(memory_score), mean, na.rm=T) %>% 
-#       arrange(group, id) %>% 
-#       mutate(group=case_when(group=="YoungKids" ~ "1", group=="OldKids" ~ "2", T ~ "3"))
-#     
-#     if (ms_session==2){
-#       d_ms_pre <- d_sm %>%
-#         filter(session==1, condition %in% ms_condition) %>%
-#         group_by(id) %>%
-#         summarise_at(vars(memory_score), mean, na.rm=T) %>%
-#         arrange(id) %>%
-#         rename(memory_score_pre=memory_score)
-# 
-#       d_ms <- d_ms %>%
-#         left_join(d_ms_pre, by="id")
-#     }
-#   } else if (ms_session==3) {
-#     # retention rate 
-#     d_ms <- d_sm %>% 
-#       filter(session %in% c(1, 2), condition %in% ms_condition) %>% 
-#       group_by(id, group, session) %>% 
-#       summarise_at(vars(memory_score), mean, na.rm=T) %>% 
-#       pivot_wider(id_cols=c("id", "group"), names_from=session, names_prefix="memory_score_", values_from="memory_score") %>% 
-#       mutate(retention_rate=memory_score_2/memory_score_1) %>% 
-#       select(-starts_with("memory_score")) %>% 
-#       arrange(group, id) %>% 
-#       mutate(group=case_when(group=="YoungKids" ~ "1", group=="OldKids" ~ "2", T ~ "3"))
-#   }
-#   
-#   # combine data
-#   d <- d_ms %>% 
-#     left_join(d_nav, by="id") %>% 
-#     left_join(d_pt, by="id")
-# 
-#   # mean imputation 
-#   # (for missing post-navigational data)
-#   d <- d %>% 
-#     impute_mean()
-#   
-#   return(d)
-# } 
-
-
 # process data for plsc analysis 
-data_for_plsc <- function(d_sm, d_pt, ms_session, ms_condition, nav_session, nav_condition, nav_trials=1:8, by_condition=TRUE){
+data_for_plsc <- function(d_sm, d_pt, ms_session, ms_condition, nav_session, nav_condition, nav_trials=1:8, by_condition=TRUE, by_session=TRUE){
   
   # data wrangling 
   # navigation
@@ -135,12 +77,12 @@ data_for_plsc <- function(d_sm, d_pt, ms_session, ms_condition, nav_session, nav
       group_by(id)
   }
   d_nav <- d_nav %>% 
-    summarise_at(vars(time, excess_path_length, excess_target_distance, initial_rotation_velocity), mean, na.rm=T)
+    summarise_at(vars(time, excess_path_length, excess_target_distance, initial_rotation), mean, na.rm=T)
   
   # memory 
   d_ms <- d_sm %>% 
     filter(session %in% ms_session, condition %in% ms_condition)
-  if (by_condition & length(ms_session)==2) {
+  if (by_condition & by_session) {
     d_ms <- d_ms %>% 
       group_by(id, session, condition, group)  %>% 
       mutate(condition=case_when(condition=="ego_ret" ~ "5", condition=="allo_ret" ~ "6", T ~ "999"))
@@ -148,9 +90,12 @@ data_for_plsc <- function(d_sm, d_pt, ms_session, ms_condition, nav_session, nav
     d_ms <- d_ms %>% 
       group_by(id, condition, group)  %>% 
       mutate(condition=case_when(condition=="ego_ret" ~ "5", condition=="allo_ret" ~ "6", T ~ "999"))
-  } 
-  else {
+  } else if (by_session){
     d_ms <- d_ms %>% 
+      group_by(id, session, group)
+  } else {
+    d_ms <- d_ms %>% 
+      mutate(session=12) %>% 
       group_by(id, session, group)
   }
   d_ms <- d_ms %>% 
@@ -185,16 +130,13 @@ plsc_allSC_by_NlS1PT <- data_for_plsc(sm_data, pt_data, ms_session=c(1,2), c("eg
 writeMat(con="../WP10_data/WP10_results/wp10_plsc_allSC_by_NlS1PT.mat", m=as.matrix(plsc_allSC_by_NlS1PT))
 rm(plsc_allSC_by_NlS1PT)
 
-plsc_allS_by_NeaS1PT <- data_for_plsc(sm_data, pt_data, ms_session=c(1,2), c("ego_ret", "allo_ret"), nav_session=1, c("ego_ret", "allo_ret"), by_condition=F)
-writeMat(con="../WP10_data/WP10_results/wp10_plsc_allS_by_NeaS1PT.mat", m=as.matrix(plsc_allS_by_NeaS1PT))
-rm(plsc_allS_by_NeaS1PT)
+plsc_all_by_NeaS1PT <- data_for_plsc(sm_data, pt_data, ms_session=c(1,2), c("ego_ret", "allo_ret"), nav_session=1, c("ego_ret", "allo_ret"), by_condition=F, by_session=F)
+writeMat(con="../WP10_data/WP10_results/wp10_plsc_all_by_NeaS1PT.mat", m=as.matrix(plsc_all_by_NeaS1PT))
+rm(plsc_all_by_NeaS1PT)
 
-plsc_allS_by_NlS1PT <- data_for_plsc(sm_data, pt_data, ms_session=c(1,2), c("ego_ret", "allo_ret"), nav_session=1, c("main_learn"), by_condition=F)
-writeMat(con="../WP10_data/WP10_results/wp10_plsc_allS_by_NlS1PT.mat", m=as.matrix(plsc_allS_by_NlS1PT))
-rm(plsc_allS_by_NlS1PT)
-
-
-# --- well-learned items 
+plsc_all_by_NlS1PT <- data_for_plsc(sm_data, pt_data, ms_session=c(1,2), c("ego_ret", "allo_ret"), nav_session=1, c("main_learn"), by_condition=F, by_session=F)
+writeMat(con="../WP10_data/WP10_results/wp10_plsc_all_by_NlS1PT.mat", m=as.matrix(plsc_all_by_NlS1PT))
+rm(plsc_all_by_NlS1PT)
 
 
 # ------------------------------------------------------------------------------
